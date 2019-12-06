a10559689371c7a5dbbfd801ba6684c8bc7900de
Richardson Bruno <richardsonbrc@gmail.com>
2019-12-06 12:56:41 -0300

    Scroll Infinito/Finito

--- src/Screen/Pages/Feed/index.js
+++ src/Screen/Pages/Feed/index.js
@@ -5,19 +5,21 @@ import {Post, Header, Avatar, Name, PostImage, Description} from './styles';
 
 export default function Feed() {
   const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
 
-  async function loadFeed() {
+  async function loadPage(pageNumber = page) {
     const response = await fetch(
-      'http://localhost:3000/feed?_expand=author&_limit=5&_page=1',
+      `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`,
     );
 
     const data = await response.json();
 
-    setFeed(data);
+    setFeed([...feed, ...data]);
+    setPage(page + 1);
   }
 
   useEffect(() => {
-    loadFeed();
+    loadPage();
   }, []);
 
   return (
@@ -25,6 +27,8 @@ export default function Feed() {
       <FlatList
         data={feed}
         keyExtractor={post => String(post.id)}
+        onEndReached={() => loadPage()}
+        onEndReachedThreshold={0.3}
         renderItem={({item}) => (
           <Post>
             <Header>
