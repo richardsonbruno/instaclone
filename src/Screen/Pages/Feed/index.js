c7d6a1faf90bd6fbfb3ea1ef7697327c78054a8b
Richardson Bruno <richardsonbrc@gmail.com>
2019-12-07 09:11:51 -0300

    Icones de carregamento

--- src/Screen/Pages/Feed/index.js
+++ src/Screen/Pages/Feed/index.js
@@ -1,26 +1,53 @@
 import React, {useState, useEffect} from 'react';
 import {View, FlatList} from 'react-native';
 
-import {Post, Header, Avatar, Name, PostImage, Description} from './styles';
+import {
+  Post,
+  Header,
+  Avatar,
+  Name,
+  PostImage,
+  Description,
+  Loading,
+} from './styles';
 
 export default function Feed() {
   const [feed, setFeed] = useState([]);
   const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
 
-  async function loadPage(pageNumber = page) {
+  async function loadPage(pageNumber = page, shouldRefresh = false) {
+    if (total && pageNumber > total) return;
+
+    setLoading(true);
+
     const response = await fetch(
       `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`,
     );
 
     const data = await response.json();
 
-    setFeed([...feed, ...data]);
+    const totalItems = response.headers.get('X-Total-Count');
+
+    setTotal(Math.floor(totalItems / 5));
+    setFeed(shouldRefresh ? data : [...feed, ...data]);
     setPage(page + 1);
+    setLoading(false);
   }
 
   useEffect(() => {
     loadPage();
   }, []);
+
+  async function refreshList() {
+    setRefreshing(true);
+
+    await loadPage(1, true);
+
+    setRefreshing(false);
+  }
 
   return (
     <View>
@@ -29,6 +56,9 @@ export default function Feed() {
         keyExtractor={post => String(post.id)}
         onEndReached={() => loadPage()}
         onEndReachedThreshold={0.3}
+        ListFooterComponent={loading && <Loading />}
+        onRefresh={refreshList}
+        refreshing={refreshing}
         renderItem={({item}) => (
           <Post>
             <Header>
